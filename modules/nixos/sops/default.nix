502d58fde226734a69a504c9405bc36002af82b2
noex <github@noex.dev>
2026-03-21 10:31:09 +0100

    fix: make sops paths persistent

--- modules/nixos/sops/default.nix
+++ modules/nixos/sops/default.nix
@@ -7,9 +7,9 @@
     defaultSopsFile = ../../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
 
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    age.sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
 
-    age.keyFile = "/var/lib/sops-nix/key.txt";
+    age.keyFile = "/persist/var/lib/sops-nix/key.txt";
 
     age.generateKey = true;
 
