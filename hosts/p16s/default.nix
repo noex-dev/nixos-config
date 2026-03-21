c430cc9b1d69ce8220be59010113a8762e827055
noex <github@noex.dev>
2026-03-21 13:50:26 +0100

    feat: add lanzaboot for host p16s

--- hosts/p16s/default.nix
+++ hosts/p16s/default.nix
@@ -11,6 +11,7 @@
     ./hardware-configuration.nix
 
     ../common
+    ../../modules/nixos/lanzaboot
     ../../modules/nixos/disko
     ../../modules/nixos/impermanence
     ../../modules/nixos/audio
