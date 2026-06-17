696ebbb85ac50e97b5867a55be01d5e7955c21d7
noex <github@noex.dev>
2026-06-17 22:11:08 +0200

    feat: split horizon vpn dns

--- modules/nixos/vpn/default.nix
+++ modules/nixos/vpn/default.nix
@@ -1,15 +1,24 @@
-{ config, lib, ... }:
-
 {
-  config = lib.mkIf config.noex.vpn.enable {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.noex.vpn;
+  dnsServer = "10.10.0.5";
+  splitDomain = "~noex.dev";
+in
+{
+  config = lib.mkIf cfg.enable {
     sops.secrets.wg_private_key = { };
     sops.secrets.wg_preshared_key = { };
 
     services.resolved.enable = true;
 
     networking.wg-quick.interfaces.vpn = {
-      address = [ config.noex.vpn.address ];
-      dns = [ "10.60.0.1" ];
+      address = [ cfg.address ];
       privateKeyFile = config.sops.secrets.wg_private_key.path;
 
       peers = [
@@ -17,10 +26,16 @@
           publicKey = "1o7FJrix5v272SdHiTr9g0KMLc/mJUXUXFQjdI2ZLQg=";
           presharedKeyFile = config.sops.secrets.wg_preshared_key.path;
           endpoint = "vpn.noex.dev:51820";
+
           allowedIPs = [ "10.0.0.0/8" ];
           persistentKeepalive = 25;
         }
       ];
+
+      postUp = ''
+        ${pkgs.systemd}/bin/resolvectl dns vpn ${dnsServer}
+        ${pkgs.systemd}/bin/resolvectl domain vpn '${splitDomain}'
+      '';
     };
   };
 }
